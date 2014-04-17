42c15a9acba264f6272fe28d44be29c53fdb78ed
Naoki Takezoe <naoki.takezoe@bizreach.co.jp>
2014-04-17 13:36:32 +0900

    Add sample echo application

--- src/main/scala/controller/IndexController.scala
+++ src/main/scala/controller/IndexController.scala
@@ -1,11 +1,27 @@
 package controller
 
 import org.scalatra._
+import jp.sf.amateras.scalatra.forms._
+import org.scalatra.json.JacksonJsonSupport
+import org.scalatra.i18n.I18nSupport
+import org.json4s.DefaultFormats
 
-class IndexController extends ScalatraFilter {
+class IndexController extends ScalatraFilter with JacksonJsonSupport with I18nSupport with ClientSideValidationFormSupport {
+
+  implicit val jsonFormats = DefaultFormats
+
+  case class HelloForm(name: String)
+
+  val form = mapping(
+    "name" -> text(required, maxlength(40))
+  )(HelloForm.apply)
 
   get("/"){
     html.index()
+  }
+
+  post("/hello", form){ form =>
+    html.hello(form.name)
   }
 
 }
